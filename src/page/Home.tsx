95c30fa9d6a1e998814e8bad24fdcf004366090f
Abishek Shah <abishekshah12315@gmail.com>
2024-02-26 10:58:03 +0545

    fix: height issue fiing

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -25,25 +25,24 @@ const Home: React.FC<Props> = ({ searchQuery }: Props) => {
 
   const onVideoClick = (videoId: string) => {
     const externalUrl = `http://localhost:5173/video-player/${videoId}`;
-    window.open(externalUrl, "_blank");
+    window.location.href = externalUrl;
   };
-
 
   return (
     <>
       <div className="relative  overflow-hidden bg-cover bg-no-repeat  grid grid-cols-1 sm:grid-cols-3 md:grid-cols- lg:grid-cols-4 gap-5 w-full">
         {searchResult?.map((data: Video, index: number) => (
           <div
-            className="flex flex-col cursor-pointer"
+            className="flex flex-col cursor-pointer "
             onClick={() => onVideoClick(getValue(data, "id.videoId"))}
             key={getValue(data, "id.videoId", index)}
           >
             <div className="w-full">
-              <img
-                src={getValue(data, "snippet.thumbnails.high.url")}
-                alt="youtube-videos"
-                className="rounded-xl hover:rounded-none hover:cursor-pointer w-full "
-              />
+            <img
+              src={getValue(data, "snippet.thumbnails.high.url")}
+              alt="youtube-videos"
+              className="rounded-xl  hover:rounded-none hover:cursor-pointer w-full  "
+            />
             </div>
 
             <div className="text-white font-medium py-2 text-xs md:text-sm">
